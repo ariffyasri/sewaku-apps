7d7838186b29890ff322b5b643c483834849c018
aiscool <ariffacm@gmail.com>
2016-04-29 19:58:04 +0800

    Revert "first time commit!"
    This reverts commit e888413c0e3f925afa345d19f1413b15f4432a8e.

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include 'connection.php';
-	unset($_SESSION['uname']);
-	unset($_SESSION['name']);
-	unset($_SESSION['email']);
-	unset($_SESSION);
-	$user->redirect('login.php?logout=success');
-
-?>
